51a4c2cb2bd4a0a84d092530febaef1a6b74d58f
jonathon <jonathonwhite@protonmail.com>
2020-06-03 11:09:25 -0400

    unraveling / raveling index

--- src/main.cu
+++ src/main.cu
@@ -23,6 +23,33 @@ struct array2d{
   }
 
 };
+struct array3d{
+  int size_0;
+  int size_1;
+  int size_2;
+  int length;
+  float* data;
+  array3d(int size_0,int size_1, int size_2)
+    :size_0(size_0),size_1(size_1),size_2(size_2)
+  {
+    length=size_0*size_1*size_2;
+    data=new float[length];
+  }
+  ~array3d(){
+    delete [] data;
+  }
+  void show(){
+    for(int _i0=0; _i0 < size_0; _i0++){
+      // printing slice
+      for(int _i1=0; _i1 < size_1; _i1++){
+        for(int _i2=0; _i2 < size_2; _i2++){
+          cout<<data[_i0*size_1*size_2 + _i1*size_2 + _i2]<<" ";
+        }cout<<endl;
+      }cout<<"------"<<endl;
+    }
+  }
+
+};
 
 __global__
 void add(array2d arr1, array2d arr2)
@@ -44,6 +71,30 @@ void add(array2d arr1, array2d arr2)
 }
 int main(void)
 {
+
+  array3d arr(3,5,5);
+  for(int i=0; i < arr.length; i++){
+    arr.data[i]=0.0;
+  }
+
+  int _i0=2;
+  int _i1=2;
+  int _i2=3;
+  arr.data[_i0*arr.size_1*arr.size_2 + _i1*arr.size_2 + _i2]=99;
+
+  int raveled_index=_i0*arr.size_1*arr.size_2 + _i1*arr.size_2 + _i2;
+  cout << "raveled_index => " << raveled_index << endl;
+  arr.show();
+  cout<<" -- "<<endl;
+  // unravel index
+  int _i_ur_0=raveled_index/(arr.size_1*arr.size_2);
+  int _i_ur_1=(raveled_index-(arr.size_1*arr.size_2*_i_ur_0))/(arr.size_2);
+  int _i_ur_2=raveled_index%arr.size_2;
+  cout << "_i_ur_0 => " << _i_ur_0 << endl;
+  cout << "_i_ur_1 => " << _i_ur_1 << endl;
+  cout << "_i_ur_2 => " << _i_ur_2 << endl;
+
+  exit(0);
   int N = 10;
   array2d arr1(N,N);
   array2d arr2(N,N);
